77eaa1361d844495befb2470f1e293cf6de691f8
Scott Vosburgh <svosburgh@stratcomm.ucla.edu>
2020-04-29 21:46:57 -0700

    remove execCommand

--- build/docs/js/scripts.js
+++ build/docs/js/scripts.js
@@ -1,11 +1,5 @@
 $(document).ready(function (){
-  function copyMe () {
-    $(document).on('click', 'code', function () {
-      $(this).addClass('active');
-      textarea = $('<textarea>').val($(this).html()).height(0).appendTo(document.body).select();
-      document.execCommand('copy');
-      textarea.remove();
-    });
-  }
-  copyMe();
+  /***************** STOP!!! READ THIS!! *****************/
+  //  Only Fractal documentation related Javascript goes here. This javascript is not exported into the components javascript. Those belong in ./src/js
+
 });
