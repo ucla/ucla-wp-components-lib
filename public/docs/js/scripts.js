79503538db9fb19436df3563fc8ca913cd23262b
Scott Vosburgh <svosburgh@stratcomm.ucla.edu>
2020-05-12 10:35:55 -0700

    - remove build and production folders

--- public/docs/js/scripts.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function (){
-  /***************** STOP!!! READ THIS!! *****************/
-  //  Only Fractal documentation related Javascript goes here. This javascript is not exported into the components javascript. Those belong in ./src/js
-
-});
