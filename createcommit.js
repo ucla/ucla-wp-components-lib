5ab0354e592692c6ba36d1a943b6ce6a43cbdf8a
Kevin Ma <kevinma@Kevins-MacBook-Pro.local>
2020-12-14 10:48:08 -0800

    adding script file to create commit update message

---
+++ createcommit.js
@@ -0,0 +1,9 @@
+// This file is used to
+const username = require("os").userInfo().username
+const fs = require('fs');
+
+const currentDate = new Date();
+
+fs.writeFile('lastcommit.txt', '"npm run commit" was last ran on: ' + currentDate + ' by: ' + username, function (err) {
+  if (err) throw err;
+});
