e6a604d1d2fa082eda67303bc916aa2d0269c743
Scott Gruber <scott@scottgruber.me>
2021-12-09 15:27:36 -0800

    use svgo.config.js via cli

---
+++ svgo.config.js
@@ -0,0 +1,23 @@
+// run from cli when necessary. gulp-image deprecated
+// `svgo -f ./src/icons/**/* -o ./public/icons/**/*`
+module.exports = {
+  multipass: true,
+  js2svg: {
+    indent: 2,
+    pretty: false,
+  },
+  plugins: [{
+    name: 'preset-default',
+    params: {
+      overrides: {
+        // disable plugins
+        removeDoctype: false,
+        removeTitle: false,
+        removeUselessStrokeAndFill: false,
+        removeViewBox: false,
+        inlineStyles: false,
+        //removeComments: false,
+      },
+    },
+  }],
+};
