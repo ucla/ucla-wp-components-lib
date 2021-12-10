dfcbb67e8e9b73d81d40b58f3aaf04f0a3dd29dc
Scott Gruber <scott@scottgruber.me>
2021-12-09 19:10:04 -0800

    add gulp-zip to devDepencies and link createcommit.js

--- createcommit.js
+++ createcommit.js
@@ -1,11 +1,11 @@
 // This file is used to
-const username = require("os").userInfo().username
+const username = require('os').userInfo().username;
 const fs = require('fs');
 
 const currentDate = new Date();
 
 fs.writeFile('lastcommit.txt', '"npm run commit" was last ran on: ' + currentDate + ' by: ' + username, function (err) {
-  if (err) throw err;
+  if (err) {throw err;}
 });
 
 
