bf1520fc558a5a7cab743aebe3e653bb00024b08
Scott Gruber <scott@scottgruber.me>
2021-12-09 19:04:30 -0800

    add commit script

--- createcommit.js
+++ createcommit.js
@@ -1,6 +1,3 @@
-// moved from package.json to save syntax
-// "commit": "npm run update \n git tag -d $(git tag -l) \nchanged=0 \ngit remote update && git status -uno | grep -q 'Your branch is behind' && changed=1\nif [ $changed = 1 ]; then\n    echo \"***WARNING***\n pull latest\";\nelse\n git-cz\nfi"
-
 // This file is used to
 const username = require("os").userInfo().username
 const fs = require('fs');
